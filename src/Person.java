ce3ccd93a7447c547318294b7660cdb625d70f92
lkoin <lkoin.kontakt@gmail.com>
2025-04-15 14:24:12 +0200

    adding age to Person

--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,20 @@
 public class Person {
     private String name;
     private String surname;
+    private int age;
 
 
-    public Person(String name, String surname) {
+    public Person(String name, String surname,int age) {
         this.name = name;
         this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
@@ -29,6 +38,7 @@ public class Person {
         return "Person{" +
                 "name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
+                ", age=" + age +
                 '}';
     }
 }
